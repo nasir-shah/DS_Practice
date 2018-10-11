38379025a5af12cacacb261ac46c704109bd5661
saednasir <tryntest123@gmail.com>
2018-10-11 10:23:06 +0000

    added a new file

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-def insertion_sort(arr):
-    for i in range(len(arr)):
-        j=i
-        while(j>0 and arr[j] < arr[j-1]):
-            temp=arr[j]
-            arr[j]=arr[j-1]
-            arr[j-1] = temp
-            j-=1
+import time
 
+def insertion_sort(arr):
+    global elapsed_time
+    start_time = time.time()
+    for i in range(len(arr)):
+        j = i
+        while(j > 0 and arr[j] < arr[j - 1]):
+            temp = arr[j]
+            arr[j] = arr[j - 1]
+            arr[j - 1] = temp
+            j -= 1
+    elapsed_time = time.time() - start_time
     return(arr)
 
 print(insertion_sort([2,4,3,5,3,56,78,32,56,789,453,33,4,56,0,8,23,45,-1,-22,33,5,465,234,5,67,8,-90]))
-print("it is now done")
+print("Time elapsed:: ", elapsed_time)
